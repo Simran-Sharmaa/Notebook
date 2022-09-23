2d614669110fcce6e91b0e20e657da38b129d7f5
Simran Sharma <simransharma1743@gmail.com>
2022-09-23 22:32:04 +0530

    Mongo Atlas

--- Backend/db.js
+++ Backend/db.js
@@ -1,8 +1,15 @@
 const mongoose= require('mongoose');
-const mongoURI='mongodb://localhost:27017/Users'
+// const mongoURI='mongodb://localhost:27017/Users'
+const mongoURI='mongodb+srv://Simran:42863@cluster0.rmaepvi.mongodb.net/notebook?retryWrites=true&w=majority'
+
 const connectToMongo=()=>{
-    mongoose.connect(mongoURI,()=>{
+    // mongoose.connect(mongoURI,()=>{
+    //     console.log("connected to mongo successfully");
+    // })
+    mongoose.connect(mongoURI)
+    .then(()=>{
         console.log("connected to mongo successfully");
     })
+    .catch(err=>console.log("not connected"))
 }
 module.exports =connectToMongo; 
